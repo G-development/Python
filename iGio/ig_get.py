331ad2018610d7634d0544c0b670237cff922e2c
Horizons-code <cuozzogiovanni03@gmail.com>
2020-09-17 02:08:23 +0200

    iGio
    Serio però

---
+++ iGio/ig_get.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"}
+
+def getUserLink(user):
+    URL = f"http://instagram.com/{user}/"
+    return URL
+
+def getStories(user):
+    userStoriesLink = f"http://instagram.com/stories/{user}/"
+
+    print(userStoriesLink)
+    return userStoriesLink
+
+
+
+
+
+#getUserLink("ciao")
+getStories("anabnormalguy")
---
+++ iGio/main.py
@@ -0,0 +1,99 @@
+import logging
+import telegram
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+
+from ig_get import *
+
+
+x = getUserLink("anabnormalguy")
+print (x)
+
+TOKEN = "1335344501:AAE9Ig5azZFQha_nvFwdu_mfT8RUxVCBwIo"
+bot = telegram.Bot(token = TOKEN)
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"}
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+welcomeMsg = """
+Heyo, welcome in iGioBot! 
+Guide coming soon... 
+Consult the command list. 
+Support me on PayPal
+"""
+
+CHOICE, USER = range(2)
+
+def start(update, context):
+    reply_keyboard = [['See stories', 'Most liked post', 'etc']]
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text = welcomeMsg)
+    update.message.reply_text(
+        'Make your choice',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+    return CHOICE
+
+
+
+def choice(update, context):
+    user = update.message.from_user
+    logger.info("Choice of %s: %s", user.first_name, update.message.text)
+
+    update.message.reply_text('Input a IG Username',
+                              reply_markup=ReplyKeyboardRemove())
+
+    
+    return USER
+
+def stories(update, context):
+    user = update.message.from_user
+    logger.info("Ig username of %s: %s", user.first_name, update.message.text)
+    update.message.reply_text('')
+
+    return ConversationHandler.END
+
+
+
+def cancel(update, context):
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    update.message.reply_text('Bye! I hope we can talk again some day.',
+                              reply_markup=ReplyKeyboardRemove())
+
+    return ConversationHandler.END
+
+def main():
+    updater = Updater(token = TOKEN, use_context=True)
+    dp = updater.dispatcher
+    
+    conv_handler = ConversationHandler(
+            entry_points=[CommandHandler('start', start)],
+
+            states={
+                CHOICE: [MessageHandler(Filters.regex('^(See stories|Most liked post|etc)$'), choice)],
+
+                USER: [MessageHandler(Filters.text, stories)]
+            },
+
+            fallbacks=[CommandHandler('cancel', cancel)]
+        )
+
+    dp.add_handler(conv_handler)
+
+    #dp.add_handler(CommandHandler("start", start))
+    #dp.add_handler(CommandHandler("favourites", favorites))
+    #dp.add_handler(CommandHandler("choice", choice))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
+
+#print(bot)
+#print(updater)
+#print(bot.get_me())
